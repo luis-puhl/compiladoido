96657e07a0835291f59d18ac3dcba9a0acb64258
luispuhl <luispuhl@gmail.com>
2014-08-29 15:27:07 -0300

    Recriado projeto a partir do Maven; Eliminado bibliotecas e estudos;

---
+++ src/main/java/si/vv/pokebola/App.java
@@ -0,0 +1,13 @@
+package si.vv.pokebola;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
